db473650d817f0f4adb3fe7a0338e90c1028420d
itsanti <kurov.aleksandr+github@gmail.com>
2018-05-11 23:30:57 +0300

    HW2: inti

---
+++ src/hw2/Runner.java
@@ -0,0 +1,63 @@
+package hw2;
+
+/**
+ * Homework #2
+ *
+ * @author Aleksandr Kurov
+ * @version dated Май 11, 2018
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+
+        // task 1
+
+
+        // task 2
+
+
+        // task 3
+
+
+        // task 4
+
+
+        // task 5
+
+        // task 6
+
+
+        // task 7
+
+    }
+
+
+    /**
+     * task 1: description
+     */
+
+    /**
+     * task 2:
+     */
+
+    /**
+     * task 3:
+     */
+
+    /**
+     * task 4:
+     */
+
+    /**
+     * task 5:
+     */
+
+    /**
+     * task 6:
+     */
+
+    /**
+     * task 7:
+     */
+
+}
