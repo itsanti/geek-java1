e0b80d2be9f4ae13778dd21c2c69e8cc7a28c619
itsanti <kurov.aleksandr+github@gmail.com>
2018-05-21 20:42:54 +0300

    HW5: init

---
+++ src/hw5/Runner.java
@@ -0,0 +1,63 @@
+package hw5;
+
+/**
+ * Homework #5
+ *
+ * @author Aleksandr Kurov
+ * @version dated Май 21, 2018
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+
+        // task 1
+
+
+        // task 2
+
+
+        // task 3
+
+
+        // task 4
+
+
+        // task 5
+
+        // task 6
+
+
+        // task 7
+
+    }
+
+
+    /**
+     * task 1: description
+     */
+
+    /**
+     * task 2:
+     */
+
+    /**
+     * task 3:
+     */
+
+    /**
+     * task 4:
+     */
+
+    /**
+     * task 5:
+     */
+
+    /**
+     * task 6:
+     */
+
+    /**
+     * task 7:
+     */
+
+}
