866c0984049cd1411f001e2b620fa7b9ae26fc64
itsanti <kurov.aleksandr+github@gmail.com>
2018-05-27 20:15:49 +0300

    HW6: init

---
+++ src/hw6/Runner.java
@@ -0,0 +1,14 @@
+package hw6;
+
+/**
+ * Homework #6
+ *
+ * @author Aleksandr Kurov
+ * @version dated Май 27, 2018
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+    }
+
+}
