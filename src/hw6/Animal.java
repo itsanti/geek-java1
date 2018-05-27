aaf8d0e84b67e79c59d18268ece892bede835aa2
itsanti <kurov.aleksandr+github@gmail.com>
2018-05-27 22:53:39 +0300

    HW6: done

---
+++ src/hw6/Animal.java
@@ -0,0 +1,56 @@
+package hw6;
+
+/**
+ * HW6 Classes
+ *
+ * @author Aleksandr Kurov
+ * @version dated Май 27, 2018
+ * @link https://github.com/itsanti
+ */
+abstract class Animal {
+    private double limitRun;
+    private double limitSwim;
+    private double limitJump;
+
+    Animal(double limitRun, double limitSwim, double limitJump) {
+        this.limitRun = limitRun;
+        this.limitSwim = limitSwim;
+        this.limitJump = limitJump;
+    }
+
+    public boolean run(double distance){
+        return distance <= limitRun;
+    }
+
+    public boolean swim(double distance){
+        return distance <= limitSwim;
+    }
+    public boolean jump(double height){
+        return height <= limitJump;
+    }
+
+    // task 5
+    public void setLimitRun(double limitRun) {
+        this.limitRun = limitRun;
+    }
+
+    public void setLimitSwim(double limitSwim) {
+        this.limitSwim = limitSwim;
+    }
+
+    public void setLimitJump(double limitJump) {
+        this.limitJump = limitJump;
+    }
+}
+
+class Cat extends Animal {
+    Cat() {
+        super(200, -1, 2);
+    }
+}
+
+class Dog extends Animal {
+    Dog() {
+        super(500, 10, 0.5);
+    }
+}
--- src/hw6/STDOUT.md
+++ src/hw6/STDOUT.md
@@ -1,3 +1,16 @@
 ```
+hw6.Cat: run(500):  false
+hw6.Cat: swim(5):   false
+hw6.Cat: jump(2):   true
 
+hw6.Dog: run(500):  false
+hw6.Dog: swim(5):   true
+hw6.Dog: jump(2):   false
+
+hw6.Dog: run(500):  true
+hw6.Dog: swim(5):   true
+hw6.Dog: jump(2):   true
+
+
+Process finished with exit code 0
 ```
