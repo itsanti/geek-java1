24fe775f10e8a29932818c5dd6943213ef5255e6
itsanti <kurov.aleksandr+github@gmail.com>
2018-05-29 19:52:34 +0300

    HW7: init

---
+++ src/hw7/Runner.java
@@ -0,0 +1,16 @@
+package hw7;
+
+/**
+ * Homework #7
+ *
+ * @author Aleksandr Kurov
+ * @version dated Май 29, 2018
+ * @link https://github.com/itsanti
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+
+    }
+
+}
